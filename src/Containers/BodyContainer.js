82529034b85bc1aa84d971a4c346d9a2c36f3fca
mkoenke <mkoenke@sas.upenn.edu>
2021-01-04 09:27:11 -0500

    started file setup

---
+++ src/Containers/BodyContainer.js
@@ -0,0 +1,14 @@
+import React from "react"
+import Body from "../Components/Body"
+
+class BodyContainer extends React.Component {
+  render() {
+    return (
+      <div>
+        <Body />
+      </div>
+    )
+  }
+}
+
+export default BodyContainer
